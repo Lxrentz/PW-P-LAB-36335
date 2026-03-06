1c5ba3d35a3b32c8a4f496c1cce56e9aa3cddd28
Henrique Lorentz <lorentzhenrique1@gmail.com>
2026-03-06 09:01:16 +0000

    Tasks

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ let movies = [
   {id: 2, title: "Interstellar", year: 2014}
 ];
 
+let tasks = [
+  { id: 1, title: "Estudar Node.js", completed: false, priority: "high" },
+  { id: 2, title: "Fazer LAB-1", completed: true, priority: "medium" }
+];
+
+// MOVIES
+
 configDotenv()
 const app = express()
 app.use(cors())
@@ -92,6 +99,120 @@ app.delete('/movies/:id', (req, res) => {
     movies = movies.filter(movies => movies.id != movieToDelete[0].id)
 
     res.status(200).json({message: "Rota de Delete acessada.", updatedMoviesList: movies})
+})
+
+// TASKS
+
+app.get('/tasks', (req, res) => {
+    const {completed} = req.query
+
+    const isCompleted = completed === "true"
+
+    let tasksToReturn = []
+
+    if(isCompleted) {
+        tasksToReturn = tasks.filter(task => task.completed == true)
+        return res.status(200).json({message: "Lista de tarefas completas acessada.", tasks: tasksToReturn})
+    } else if(!isCompleted) {
+        tasksToReturn = tasks.filter(task => task.completed == false)
+        return res.status(200).json({message: "Lista de tarefas inacabadas acessada.", tasks: tasksToReturn})
+    }
+
+    res.status(200).json({message: "Lista de tarefas acessada.", tasks: tasks})
+})
+
+app.get('/tasks/:id', (req, res) => {
+
+    const { 
+        id
+    } = req.params
+    
+    if (!id) {
+        return res.status(404).json({message: "Nenhum ID foi adicionado."})
+    }
+
+    const task = tasks.find((task) => task.id == id)
+
+    if (!task) {
+        return res.status(404).json({message: "Nenhuma tarefa com este ID foi encontrada."})
+    }
+
+    res.status(200).json({message: "Sucesso ao acessar a rota de Get Tasks.", task})
+})
+
+app.post('/tasks', (req, res) => {
+
+    const tasksLenght = tasks.length
+
+    const {
+        title,
+        priority
+    } = req.body
+
+    if (!title || !priority) {
+        res.status(404).json({message: "Erro! Introduza o título da tarefa e a sua prioridade."})
+    }
+
+    let completed = false
+
+    const newTask = {
+        id: tasksLenght + 1,
+        title,
+        priority,
+        completed
+    }
+
+    tasks.push(newTask)
+
+    res.status(200).json({message: "Nova tarefa adicionada.", tasks: tasks})
+})
+
+app.put('/tasks/:id', (req, res) => {
+    
+    const {id} = req.params
+    const {title, priority} = req.body
+
+    const taskToEdit = tasks.filter(task => task.id == id)
+
+    if (title) {
+        taskToEdit[0].title = title
+    } 
+    if (priority) {
+        taskToEdit[0].year = year
+    }
+
+    if (!title && !priority) {
+        return res.status(404).json({message: "Erro! Nenhum dado foi passado para realizar a edição."})
+    }
+
+    res.status(200).json({message: `A tarefa de ID ${id} foi editada.`, updatedTasksList: tasks})
+})
+
+app.patch("/tasks/:id/toggle", (req, res) => {
+    const {id} = req.params
+
+    const task = tasks.find(task => task.id == id)
+
+    task.completed = !task.completed
+
+    if(!task) {
+        return res.status(404).json({message: "Não foi possível encontrar a tarefa."})
+    }
+
+    else {
+        return res.status(200).json({message: "Rota encontrada com sucesso."})
+    }
+})
+
+app.delete('/tasks/:id', (req, res) => {
+
+    const {id} = req.params
+
+    const taskToDelete = tasks.filter(task => task.id == id)
+    
+    tasks = tasks.filter(tasks => tasks.id != taskToDelete[0].id)
+
+    res.status(200).json({message: "Rota de Delete acessada.", updatedTasksList: tasks})
 })
 
 
